afacbfe6b3750f8eac92cb03f50b62d67d93b290
Alejandro Mendez Rojas <amr@Alejandros-MacBook-Pro.local>
2024-03-05 13:43:13 -0600

    now hooks are used instead of classes

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,55 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css'
 
-class App extends React.Component {
-	constructor () {
-		super();
-		this.state = {
-			robots: [],
-			searchfield: ''
-		}
-	}
+function App() {
+	// constructor () {
+	// 	super();
+	// 	this.state = {
+	// 		robots: [],
+	// 		searchfield: ''
+	// 	}
+	// }
 
-	componentDidMount() {
+	// You pass the state thing and the function to modify it. The parameter of useState
+	// is the initial value of the thing
+	const [robots, setRobots] = useState([]);
+	const [searchfield, setSearchfield] = useState("");
+
+	// componentDidMount() {
+	// 	fetch('https://jsonplaceholder.typicode.com/users')
+	// 		.then(response => response.json())
+	// 		.then(users => this.setState({robots: users}));
+	// 	// this.setState({ robots: robots})
+	// }
+
+	// useEffect gets run every time the app is run
+	// The first parameter is the effect. The second one,
+	// if present, effect will only activate if the values in the list change
+	// If the list is empty, then it will run only the first time
+	useEffect(() => {
 		fetch('https://jsonplaceholder.typicode.com/users')
 			.then(response => response.json())
-			.then(users => this.setState({robots: users}));
-		// this.setState({ robots: robots})
-	}
+			.then(users => setRobots(users));
+			// console.log('robots and searchfield ', robots, searchfield);
+	}, [])
 
 	// Defining onSearchChange this way instead of 
 	// onSearchChange(event) => {}
 	// allows us to use --this-- inside
-	onSearchChange = (event) => {	
-		this.setState({searchfield: event.target.value});
+	const onSearchChange = (event) => {	
+		setSearchfield(event.target.value);
 	}
 
-	render () {
-		const { robots, searchfield} = this.state;
-		const filteredRobots = robots.filter(robot => {
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-		})
-		// Equivalent to robots.length === 0
-		if (!robots.length) {
-			return <h1>Loading</h1>
-		}else{
-			return (
-				<div className='tc'>
-					<h1 className='f1'> RoboFriends </h1>
-					<SearchBox searchChange={this.onSearchChange}/>
-					<Scroll>
-						<ErrorBoundry>
-							<CardList robots={filteredRobots}/>
-						</ErrorBoundry>
-					</Scroll>
-				</div>
-			);
-		}
+
+	const filteredRobots = robots.filter(robot => {
+		return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+	})
+	// Equivalent to robots.length === 0
+	
+	// console.log('searchfield ', searchfield);
+	if (!robots.length) {
+		return <h1>Loading</h1>
+	}else{
+		return (
+			<div className='tc'>
+				<h1 className='f1'> RoboFriends </h1>
+				<SearchBox searchChange={onSearchChange}/>
+				<Scroll>
+					<ErrorBoundry>
+						<CardList robots={filteredRobots}/>
+					</ErrorBoundry>
+				</Scroll>
+			</div>
+		);
 	}
+	
 	
 }
 
